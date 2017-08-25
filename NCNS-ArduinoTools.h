68e25dc8e11a2959e5229f87066b03eb3bd68b58
Pierre-Gilles Levallois <pgl@erasme.org>
2017-08-25 14:50:12 +0200

    ajout d'une fonction ledBlink. ErrorBlink est conservée pour compatibilité ascendante.

--- NCNS-ArduinoTools.h
+++ NCNS-ArduinoTools.h
@@ -5,17 +5,18 @@
 // ----------------------------------------------------------
 // Do blink a led without delay
 // ----------------------------------------------------------
-void errorBlink(unsigned int _led, unsigned int _delayMs) {
-
+void ledBlink(unsigned int _led, unsigned int _delayMs) {
   float alternativeSignal = (millis() % _delayMs) / (float)_delayMs;
-
-//  Serial.print("Alternative signal : ");
-//  Serial.println(alternativeSignal);
-
   if (alternativeSignal > 0.5) {
     digitalWrite(_led, HIGH);
   } else {
     digitalWrite(_led, LOW);
   }
+}
 
+// ----------------------------------------------------------
+// For backward compatibility
+// ----------------------------------------------------------
+void errorBlink(unsigned int _led, unsigned int _delayMs) {
+	ledBlink( _led, _delayMs);
 }
