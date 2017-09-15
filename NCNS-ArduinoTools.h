c60b1b83ba3c4ba4f08ea2b6284009c9e0417d6c
Pierre-Gilles Levallois <pgl@erasme.org>
2017-09-15 10:19:28 +0200

    ajout de 2 fonctions de read et write dans l'EEPROM

--- NCNS-ArduinoTools.h
+++ NCNS-ArduinoTools.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
+#include <EEPROM.h>
 
 // ----------------------------------------------------------
 // Do blink a led without delay
@@ -20,3 +21,28 @@ void ledBlink(unsigned int _led, unsigned int _delayMs) {
 void errorBlink(unsigned int _led, unsigned int _delayMs) {
 	ledBlink( _led, _delayMs);
 }
+
+
+/*************************************************
+   Writing a float in EEPROM
+ **************************************************/
+void eeprom_write(int addr, float f) {
+  unsigned char *buf = (unsigned char*)(&f);
+  for ( int i = 0 ; i < (int)sizeof(f) ; i++ ) {
+    EEPROM.write(addr + i, buf[i]);
+  }
+  EEPROM.commit();
+}
+
+/*************************************************
+   Reading a float from EEPROM
+ **************************************************/
+float eeprom_read(int addr) {
+  float f;
+  unsigned char *buf = (unsigned char*)(&f);
+  for ( int i = 0 ; i < (int)sizeof(f) ; i++ ) {
+    buf[i] = EEPROM.read(addr + i);
+  }
+  return f;
+}
+
